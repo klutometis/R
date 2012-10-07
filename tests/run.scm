96644761b35790a44c1d6a8ff0855a1f044b75d0
Peter Danenberg <pcd@roxygen.org>
2012-10-07 07:19:55 -0700

    Use the new syntax.

--- tests/run.scm
+++ tests/run.scm
@@ -2,71 +2,71 @@
 
 (test "Pi via named arguments to get"
       3.14159265358979
-      (R* get x: "pi"))
+      (R* (get x: "pi")))
 
 (test "Scalar integer"
       2
-      (R* c 2))
+      (R* 2))
 
 (test "Vector integers"
       '#(2 3)
-      (R* c 2 3))
+      (R* (c 2 3)))
 
 (test "Nested vector-integers"
       '#(2 3 4 5)
-      (R* c 2 3 (R c 4 5)))
+      (R* (c 2 3 (c 4 5))))
 
 (test "Nested list-integers"
       '(2 3 (4 5))
-      (R* list 2 3 (R list 4 5)))
+      (R* (list 2 3 (list 4 5))))
 
 (test "String scalar"
       "harro"
-      (R* c "harro"))
+      (R* "harro"))
 
 (test "String vector"
       '#("harro" "harro")
-      (R* rep "harro" 2))
+      (R* (rep "harro" 2)))
 
 (test "Scalar real"
       2.1
-      (R* c 2.1))
+      (R* 2.1))
 
 (test "Scalar vector"
       '#(2.1 2.1)
-      (R* rep 2.1 2))
+      (R* (rep 2.1 2)))
 
 (test "Scalar boolean: false"
       #f
-      (R* c #f))
+      (R* #f))
 
 (test "Scalar boolean: true"
       #t
-      (R* c #t))
+      (R* #t))
 
 (test "Vector boolean"
       '#(#f #f)
-      (R* rep #f 2))
+      (R* (rep #f 2)))
 
 ;; Doesn't respect exactness; i.e., complex numbers are represented
 ;; by doubles in R.
 (test "Scalar complex"
       (make-rectangular 3.0 3.0)
-      (R* c (make-rectangular 3 3)))
+      (R* (c (make-rectangular 3 3))))
 
 (test "Vector complex"
       (make-vector 2 (make-rectangular 3.0 3.0))
-      (R* rep (make-rectangular 3 3) 2))
+      (R* (rep (make-rectangular 3 3) 2)))
 
 (test "Named arguments"
       '#(3 3)
-      (R* rep.int times: 2 x: 3))
+      (R* (rep.int times: 2 x: 3)))
 
-(let ((env (R new.env R-missing)))
-  (R assign "a" 2 pos: env)
+(let ((env (R (new.env))))
+  (R (assign "a" 2 pos: env))
   (test-assert "Opaque object"
-               (R* exists "a" R-missing env)))
+               (R* (exists "a" R-missing env))))
 
 (test "Attributes"
       '#("a" "b")
-      (R* attr (R list a: 1 b: 2) "names"))
+      (R* (attr (list a: 1 b: 2) "names")))
