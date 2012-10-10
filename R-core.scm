6be5aee1626bdea1707d7da08eaaf0d6dda5590b
Peter Danenberg <pcd@roxygen.org>
2012-10-10 14:14:04 -0700

    Have to add define-record-and-printer for the time being.

--- R-core.scm
+++ R-core.scm
@@ -78,6 +78,28 @@ END
          "R_ReleaseObject((SEXP) sexp);")
         sexp)))))
 
+(define-syntax define-record-and-printer
+  @("Define a record and a printer that shows the contents of the
+record as a vector; see {{define-record}}."
+    (name "The name of the record")
+    (slotname "The name of the slot")
+    (@to "record"))
+  (lambda (expression rename compare)
+    (match expression
+      ((_ record . fields)
+       (let ((%define-record (rename 'define-record))
+             (%define-record-printer (rename 'define-record-printer))
+             (%begin (rename 'begin))
+             (%lambda (rename 'lambda))
+             (%write (rename 'write))
+             (%record->vector (rename 'record->vector)))
+         `(,%begin
+           (,%define-record ,record ,@fields)
+           (,%define-record-printer
+            ,record
+            (,%lambda (record out)
+                 (,%write (,%record->vector record) out)))))))))
+
 (define-record-and-printer NA)
 (define NA (make-NA))
 (define (R-NA? sexp)
